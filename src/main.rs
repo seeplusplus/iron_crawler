5a7d5f06a92c88179948e4a8c127c8fec0690a21
Caleb Webber <caleb.webber@unos.org>
2020-11-16 18:00:46 -0500

    Continue following tutorials

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 //! Pong Tutorial 1
-
+use amethyst::core::transform::TransformBundle;
 use amethyst::{
     prelude::*,
     renderer::{
@@ -9,10 +9,8 @@ use amethyst::{
     },
     utils::application_root_dir,
 };
-
-pub struct Pong;
-impl SimpleState for Pong {}
-
+mod pong; 
+use crate::pong::Pong;
 
 extern crate amethyst;
 use amethyst::prelude::*;
@@ -25,17 +23,18 @@ fn main() -> amethyst::Result<()> {
     let display_config_path = app_root.join("config").join("display.ron");
 
     let game_data = GameDataBuilder::default()
-        .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
-                .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)?
-                        .with_clear([0.0, 0.0, 0.0, 1.0]),
-                )
-                // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
-                .with_plugin(RenderFlat2D::default()),
-        )?;
-
+    .with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+        // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+        .with_plugin(
+            RenderToWindow::from_config_path(display_config_path)?
+            .with_clear([0.0, 0.0, 0.0, 1.0]),
+        )
+        // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+        .with_plugin(RenderFlat2D::default()),
+    )?
+    .with_bundle(TransformBundle::new())?;
+    
     let assets_dir = app_root.join("assets");
     let mut game = Application::new(assets_dir, Pong, game_data)?;
     game.run();
---
+++ src/pong.rs
@@ -0,0 +1,117 @@
+use amethyst::{
+    assets::{AssetStorage, Loader, Handle},
+    core::transform::Transform,
+    ecs::{Component, DenseVecStorage},
+    prelude::*,
+    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
+};
+
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+fn initialise_camera(world: &mut World) {
+    // Setup camera in a way that our screen covers whole arena and (0, 0) is in the bottom left.
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(ARENA_WIDTH * 0.5, ARENA_HEIGHT * 0.5, 1.0);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
+        .with(transform)
+        .build();
+}
+/// Initialises one paddle on the left, and one paddle on the right.
+fn initialise_paddles(world: &mut World,  sprite_sheet_handle: Handle<SpriteSheet>) {
+    // Assign the sprites for the paddles
+    let sprite_render = SpriteRender::new(sprite_sheet_handle, 0);  // paddle is the first sprite in the sprite_sheet
+
+    let mut left_transform = Transform::default();
+    let mut right_transform = Transform::default();
+
+    // Correctly position the paddles.
+    let y = ARENA_HEIGHT / 2.0;
+    left_transform.set_translation_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
+    right_transform.set_translation_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
+
+    // Create a left plank entity.
+    world
+        .create_entity()
+        .with(sprite_render.clone())
+        .with(Paddle::new(Side::Left))
+        .with(left_transform)
+        .build();
+
+    // Create right plank entity.
+    world
+        .create_entity()
+        .with(sprite_render.clone())
+        .with(Paddle::new(Side::Right))
+        .with(right_transform)
+        .build();
+}
+
+pub struct Pong;
+impl SimpleState for Pong {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        // Load the spritesheet necessary to render the graphics.
+        let sprite_sheet_handle = load_sprite_sheet(world);
+        world.register::<Paddle>();
+
+        initialise_paddles(world, sprite_sheet_handle);
+        initialise_camera(world);
+    }
+}
+
+pub const PADDLE_HEIGHT: f32 = 16.0;
+pub const PADDLE_WIDTH: f32 = 4.0;
+
+#[derive(PartialEq, Eq)]
+pub enum Side {
+    Left, 
+    Right,
+}
+
+pub struct Paddle {
+    side: Side,
+    width: f32,
+    height: f32,
+}
+
+impl Paddle {
+    fn new (side: Side) -> Paddle {
+        Paddle {
+            side,
+            width: PADDLE_WIDTH,
+            height: PADDLE_HEIGHT,
+        }
+    }
+}
+
+impl Component for Paddle {
+    type Storage = DenseVecStorage<Self>;
+}
+
+fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
+    // Load the sprite sheet necessary to render the graphics.
+    // The texture is the pixel data
+    // `texture_handle` is a cloneable reference to the texture
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "texture/pong_spritesheet.png",
+            ImageFormat::default(),
+            (),
+            &texture_storage,
+        )
+    };
+    let loader = world.read_resource::<Loader>();
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+        "texture/pong_spritesheet.ron", // Here we load the associated ron file
+        SpriteSheetFormat(texture_handle),
+        (),
+        &sprite_sheet_store,
+    )
+}
